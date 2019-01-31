c0f175f2cd4af9e7206b0d7526cd86d84744eb2e
VlogP <38265453+VlogP@users.noreply.github.com>
2019-01-31 19:11:13 +0300

    For Home2

--- FinalItechArt/App/index.jsx
+++ FinalItechArt/App/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import{Provider,connect}from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware,compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import {put, call,takeEvery} from 'redux-saga/effects';
 
+
 // Reducer
 const initialState = {
-  url: '',
-  loading: false,
+  Name:'',
+  Password:'',
   error: false,
 };
 const reducer = (state = initialState, action) => {
@@ -21,8 +22,7 @@ const reducer = (state = initialState, action) => {
       };
     case 'REQUESTED_DOG_SUCCEEDED':
       return {
-        url: action.url,
-        loading: false,
+        Password: action.Password,
         error: false,
       };
     case 'REQUESTED_DOG_FAILED':
@@ -42,20 +42,38 @@ const requestDog = () => {
 };
 
 const requestDogSuccess = (data) => {
-  return { type: 'REQUESTED_DOG_SUCCEEDED', url: data.message }
+  return { type: 'REQUESTED_DOG_SUCCEEDED', url: data.Password }
 };
 
 const requestDogError = () => {
   return { type: 'REQUESTED_DOG_FAILED' }
 };
 
-const fetchDog = () => {
-  return { type: 'FETCHED_DOG' }
-};
+
+function validateAge(age){
+  var myRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[!-+.@#$%^&*]).{8,15}$/;
+  var myArray = myRe.test(age);
+    return myArray;
+}
+function validateName(name){
+    return name.length>2;
+}
+function onAgeChange(e) {
+  var val = e.target.value;
+  var valid = this.validateAge(val);
+  this.setState({age: val, ageValid: valid});
+}
+function onNameChange(e) {
+  var val = e.target.value;
+  console.log(val);
+  var valid = this.validateName(val);
+  this.setState({name: val, nameValid: valid});
+}
 
 // Sagas
 function* watchFetchDog() {
-  yield takeEvery('FETCHED_DOG', fetchDogAsync);
+  yield takeEvery('FETCHED_DOG', PasswordAsync);
+
 }
 
 function* fetchDogAsync() {
@@ -74,38 +92,73 @@ function* fetchDogAsync() {
 
 // Component
 class App extends React.Component {
+  
+  componentWillMount(){
+
+  }
+  componentDidMount() {
+   
+};
+
   render () {
     return (
-      <div>
-        <button onClick={() => this.props.dispatch(fetchDog())}>Show Dog</button>
-          {this.props.loading 
-            ? <p>Loading...</p> 
-            : this.props.error
-                ? <p>Error, try again</p>
-                : <p><img src={this.props.url}/></p>}
-      </div>
+      <form onSubmit={this.props.fetchDog}>
+      <p>
+          <label>Имя:</label><br />
+          <input type="text" value={this.state.Name} 
+              onChange={this.onNameChange} style={{borderColor:nameColor}} />
+      </p>
+      <p>
+          <label>Возраст:</label><br />
+          <input type="text" value={this.state.age} 
+              onChange={this.onAgeChange}  style={{borderColor:ageColor}} />
+      </p>
+      <input type="submit" value="Отправить" />
+  </form>
     )
   }
 }
 
 // Store
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware)
-);
 
-sagaMiddleware.run(watchFetchDog);
 
-const ConnectedApp = connect((state) => {
-  console.log(state);
-  return state;
-})(App);
+
+
+const mapStateToProps = state => {
+  return {
+    url: state.url,
+    loading: state.loading,
+    error:state.error
+  };
+};
+
+const mapDispatchToProps =dispatch =>{
+  return{
+  fetchDog: () => dispatch({ type: "FETCHED_DOG" })
+  
+  };
+};
+
+
+export default App=connect(mapStateToProps, mapDispatchToProps)(App); 
+
+
+
+
+
+ const sagaMiddleware = createSagaMiddleware();
+
+ const store = createStore(
+   reducer,
+   applyMiddleware(sagaMiddleware)
+ );
+ 
+ sagaMiddleware.run(watchFetchDog);
 
 // Container component
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedApp />
+    <App />
   </Provider>,
   document.getElementById('content')
 );
